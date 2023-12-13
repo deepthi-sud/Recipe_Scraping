892d3552f7e5494d05a8a681b1eff43ecb7427ed
swapna1506 <swapnay1506@gmail.com>
2023-12-13 14:53:25 -0600

    first commit

--- src/test/java/com/recipe/testCases/TC_002_HypoThyroidism.java
+++ src/test/java/com/recipe/testCases/TC_002_HypoThyroidism.java
@@ -1,7 +1,17 @@
 package com.recipe.testCases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import com.recipe.testBase.BaseClass;
 
 public class TC_002_HypoThyroidism extends BaseClass{
+	
+	public void hypoThyrodism()
+	{
+		WebElement clickOnAtoZ=driver.findElement(By.xpath("//div[@id='toplinks']/a[5]"));
+		clickOnAtoZ.click();
+		logger.info("clicked on Recipe A to Z");
 
+}
 }
