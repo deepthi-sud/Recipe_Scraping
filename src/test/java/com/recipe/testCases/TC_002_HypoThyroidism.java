29798fafbd7def3786d08a0b91dfab9284230051
Deepthi <Deepthi@10.0.0.72>
2023-12-18 12:31:57 -0500

    commit after changes with excel write

--- src/test/java/com/recipe/testCases/TC_002_HypoThyroidism.java
+++ src/test/java/com/recipe/testCases/TC_002_HypoThyroidism.java
@@ -4,207 +4,250 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.NoSuchElementException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.NotFoundException;
-
 import com.recipe.testBase.BaseClass;
-import com.recipe.utilities.ExcelReadAndWrite;
+
+import com.recipe.utilities.ExcelUtility;
 
 public class TC_002_HypoThyroidism extends BaseClass {
-
-	static int flag = 0;
+	static int flag, allergyFlag = 0;
+	private com.recipe.utilities.ExcelUtility excel;
 	int eliminated_recipe_count =  0;
+	int goodRecipes_count=0;
 	Instant timer_start, timer_end;
+	String allergy_name;
 
-	public enum FoodCategory {
-		VEGETARIAN , VEGAN,EGGETARIAN, JAIN
-
-	}
-
-	public enum RecipeCategory {
-		NONE, BREAKFAST ,LUNCH, SNACK , DINNER
-	}
-
-	public enum Comorbidity {
-		DIABETES , PCOS , HYPOTHYROIDISM , HYPERTENSION
-	}
+	public String recipeID;
+	public String Recipe_name;
+	public String ingredients;
+	public String prep_method;
+	public String PrepTime;
+	public String CookTime;
+	public String recipe_url;
+	public String nutrient;
+	public String recipeCategory;
+	public String targetmorbidity;
+	public String foodCategory;
 
 	@Test
 	public  void hypoThyrodism() throws Exception
 
 	{
+		ExcelUtility  xlutil = new ExcelUtility(".\\TestData\\Team7_Scraping_Spatulas_ScrapedRecipes.xlsx");
+		ArrayList<String> EliminateList = excel.readDataFromSheet(0, 6);
+		//		ArrayList<String> toAddList = excel.readDataFromSheet(0, 7);
+		//	ArrayList<String> allergyList = excel.readDataFromSheet(1, 0);
+
+
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 0, "RecipeID");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 1, "RecipeName");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 2, "RecipeCategory");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 3, "FoodCategory");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 4, "Preparation Time");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 5, "Cooking Time");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 6, "Ingredients");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 7, "RecipeMethod");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 8, "Targetted morbidity");
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 9, "Recipe URL");				
+		xlutil.setCellData("HYPOTHYROIDISM", 0, 10, "Nutrients");
+
 		timer_start = Instant.now();
 		int eliminated_recipe_count =  0;
 		int total_recipes = 0;
 
-
-		ArrayList<String> EliminatedList = ExcelReadAndWrite.readEliminatedList(2);
-
-
 		//Click on Recipes A to Z
 		WebElement recipes=driver.findElement(By.xpath("//div[@id = 'toplinks']/a[5]"));
 		recipes.click();
+		//commonmethods.clickRecipesAtoZ();
 		logger.info("clicked on Recipe A to Z");
 
-		//click on alphabets
-		WebElement alphab=driver.findElement(By.xpath("//table[@id='ctl00_cntleftpanel_mnuAlphabets']//td[5]"));
-		alphab.click();
+		List<WebElement> getAlphabates=driver.findElements(By.xpath("//table[@id='ctl00_cntleftpanel_mnuAlphabets']//td[position()>=3 and position()<last()]//a"));
+		int alphabetCount=getAlphabates.size();
+		System.out.println("total alphabetes: "+alphabetCount);
+		if (alphabetCount > 0)
+		{
+			for(int a=0;a<alphabetCount;a++)
+			{
+				WebElement active_alphabet = driver.findElement(By.xpath("//table[@id='ctl00_cntleftpanel_mnuAlphabets']//td[position()=last()]//a[text()='" + getAlphabates.get(a).getText() + "']"));
+				System.out.println("Active alphabet: " + active_alphabet.getText());
+				active_alphabet.click();
 
-		List <WebElement> recipe_link_pages = driver.findElements(By.xpath("//a[@class='respglink']"));
-		int num =recipe_link_pages.size();
-		System.out.println(num);
-		String total_pages_to_scrape = recipe_link_pages.get(num-1).getText();
-		int total_pages = (Integer.parseInt(total_pages_to_scrape));
-		System.out.println("Total pages to Scrape: "+total_pages);
+				List <WebElement> pages = driver.findElements(By.xpath("//div[@style='text-align:right;padding-bottom:15px;'][1]/a"));
 
-		//Traverse through pages
+				int pageCount =pages.size();
+				System.out.println("page count is: " +pageCount);
+				String total_pages_to_scrape = pages.get(pageCount-1).getText();
+				int total_pages = (Integer.parseInt(total_pages_to_scrape));
+				System.out.println("Total pages to Scrape: "+total_pages);
+				//Traverse through pages
 
-		for(int p=1; p<=5;p++) {
-			String current_url = driver.getCurrentUrl();
-			System.out.println(current_url);
+				for(int p=1; p<=total_pages;p++) 
+				{
 
-			WebElement active_page=driver.findElement(By.xpath("//div[@style='text-align:right;padding-bottom:15px;'][1]/a[text()='"+p+"']"));
-			System.out.println("active page: "+active_page.getText());        
-			active_page.click();
+					WebElement active_page=driver.findElement(By.xpath("//div[@style='text-align:right;padding-bottom:15px;'][1]/a[text()='"+p+"']"));
+					System.out.println("active page: "+active_page.getText());        
+					active_page.click();
 
-			List <WebElement> recipeLinks=driver.findElements(By.xpath("//div[@class='rcc_recipecard']//div[3]/span/a"));
-			int recipeLinksSize=recipeLinks.size();
+					List <WebElement> recipeLinks=driver.findElements(By.xpath("//div[@class='rcc_recipecard']//div[3]/span/a"));
+					int recipeLinksSize=recipeLinks.size();
 
-			System.out.println("Total Recipe cards in Page"+p+":"+recipeLinksSize);
-			try {
-				//Traverse through recipes in each page
-				for_loop:
-					for(int j=0;j<recipeLinksSize; j++ )
-					{
+					System.out.println("Total Recipe cards in Page "+p+":"+recipeLinksSize);
+
+					//Traverse through recipes in each page
+					try {
+						for_loop:
+							if(recipeLinksSize>0) {
+								for(int j=0;j<recipeLinksSize; j++ )
+								{         
+									System.out.println("Loop index j: " + j);
+									JavascriptExecutor js = (JavascriptExecutor) driver;
+									js.executeScript("window.scrollBy(0,350)", "");
+
+									String Recipe_name = recipeLinks.get(j).getText();
+
+									recipeLinks.get(j).click();
+									String recipe_url = driver.getCurrentUrl();
+									String ingredients = driver.findElement(By.xpath("//div[@id='rcpinglist']")).getText();
+
+									flag = 0;
+									outer:
+										for(int k=0;k<EliminateList.size();k++)
+										{
+											String s1 =EliminateList.get(k).toLowerCase();
+											String s2 = ingredients.toLowerCase();
+											if(s2.contains(s1))
+											{
+												eliminated_recipe_count++;
+												flag = 1;
+												break outer;
+											}
+
+										}
+
+									//	            outer1:
+									//	            	for (String betterIngrediants : toAddList) 
+									//	            	{
+									//	    				if (ingredients.toLowerCase().contains(betterIngrediants.trim())) 
+									//	    				{
+									//	    					goodRecipes_count++;
+									//	    					System.out.println("GOOD TO HAVE RECIPE: " + Recipe_name);
+									//	    					break outer1;
+									//	    				}
+									//	    			}
+									//            	
+									//	            	outer2:
+									//	            		 for(int k=0;k<allergyList.size();k++)
+									//	                     {
+									//	                   	  String s1 = allergyList.get(k).toLowerCase();
+									//	                   	  String s2 = ingredients.toLowerCase();
+									//	                   	  if(s2.contains(s1))
+									//	                   	  {
+									//	                
+									//	                   		  allergyFlag = 1;
+									//	                   		  allergy_name = s1;
+									//	                   		  System.out.println("ALLERGY TYPE: "+allergy_name+"            RECIPE NAME:       " + Recipe_name);
+									//	                   		  break outer2;
+									//	                         }
+									//	                     }
+									//	            	
+
+									if(flag==0)
+									{
+										System.out.println("********************************************************");
+										System.out.println(j+" : Recipe name: " +Recipe_name);
+										System.out.println(recipe_url);
+										String recipeID= recipe_url.replaceAll("[^0-9]", "").toString();
+										System.out.println("Recipe ID: "+ recipeID);
+										System.out.println("****Ingredients****");
+
+										System.out.println(ingredients);
+										System.out.println("------------------------");
+										try {
+											WebElement prep_time = driver.findElement(By.xpath("//time[@itemprop = 'prepTime']"));
+											String PrepTime=prep_time.getText().toString();
+											xlutil.setCellData("HYPOTHYROIDISM", j, 4, PrepTime);
+											System.out.println("PreparationTime: "+PrepTime);
+
+											WebElement cook_time=driver.findElement(By.xpath("//time[@itemprop = 'cookTime']"));
+											String CookTime = cook_time.getText().toString();
+											xlutil.setCellData("HYPOTHYROIDISM", j, 5, CookTime); 
+											System.out.println("CookingTime: "+CookTime);
+
+										}	catch(NoSuchElementException e)
+										{
+											System.err.println("Element not found: //time[@itemprop = 'prepTime']");
+											System.err.println("Element not found: //time[@itemprop = 'cookTime']");
+										}
+										WebElement method = driver.findElement(By.xpath("//div[@id='recipe_small_steps']"));
+										System.out.println("----------Recipe Method: ----------------------------------------------------------------------");
+										String prep_method =method.getText().toString();
+										//xlutil.setCellData("Sheet2", j, 7, prep_method);
+										System.out.println(prep_method);
+
+										try {
+											WebElement nutrients = driver.findElement(By.xpath("//table[@id='rcpnutrients']"));
+											xlutil.setCellData("HYPOTHYROIDISM", j, 10, nutrient);
+											String nutrient = nutrients.getText();
+											System.out.println("----------nutrients:------------------------------------ " + nutrient);
+										}catch(NoSuchElementException e)
+										{
+											System.err.println("Element not found: //table[@id='rcpnutrients']");
+										}
 
 
-						JavascriptExecutor js = (JavascriptExecutor) driver;
-						js.executeScript("window.scrollBy(0,350)", "");
+										// String tags = driver.findElement(By.xpath("//div[@id = 'recipe_tags']")).getText();
+										System.out.println("------------------------");
+										foodCategory=getFoodCategory().toString();
+										System.out.println("FoodCategory: "+foodCategory);
+										System.out.println("------------------------");
+										recipeCategory=getRecipeCategory().toString();
+										System.out.println("RecipeCategory: "+recipeCategory);
+										System.out.println("------------------------");
+										targetmorbidity=SetHypoThyroidismMorbidity().toString();
+										System.out.println("TargettedMorbidity: "+targetmorbidity);
 
 
-						String Recipe_name = recipeLinks.get(j).getText();
-
-						recipeLinks.get(j).click();
-
-						String ingredients = driver.findElement(By.xpath("//div[@id='rcpinglist']")).getText();
-
-						flag = 0;
-						outer:
-							for(int k=0;k<EliminatedList.size();k++)
-							{
-								String s1 =EliminatedList.get(k).toLowerCase();
-								String s2 = ingredients.toLowerCase();
-								if(s2.contains(s1))
-								{
-									eliminated_recipe_count++;
-									flag = 1;
-									break outer;
+										xlutil.setCellData("HYPOTHYROIDISM", j, 0, recipeID);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 1, Recipe_name);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 2, recipeCategory);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 3, foodCategory);
+										//xlutil.setCellData("HYPOTHYROIDISM", j, 4, PrepTime);
+										//xlutil.setCellData("HYPOTHYROIDISM", j, 5, CookTime);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 6, ingredients);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 7, prep_method);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 8, targetmorbidity);
+										xlutil.setCellData("HYPOTHYROIDISM", j, 9, recipe_url);
+									//	xlutil.setCellData("HYPOTHYROIDISM", j, 10, nutrient);
+									}
+									driver.navigate().back();
+									recipeLinks=driver.findElements(By.xpath("//div[@class='rcc_recipecard']//div[3]/span/a"));
+									//end of recipes loop
 								}
-
-							}
-
-						if(flag==0)
-						{
-							System.out.println("********************************************************");
-							System.out.println("Recipe name: " +Recipe_name);
-							String recipe_url = driver.getCurrentUrl();
-							String recipe_id = recipe_url.replaceAll("[^0-9]", "");
-							System.out.println("Recipe ID: "+ recipe_id);
-							System.out.println("****Ingredients****");
-
-							System.out.println(ingredients);
-							System.out.println("------------------------");
-							String PrepTime = driver.findElement(By.xpath("//time[@itemprop = 'prepTime']")).getText();
-							System.out.println("PreparationTime: "+PrepTime);
-							String CookTime = driver.findElement(By.xpath("//time[@itemprop = 'cookTime']")).getText();
-							System.out.println("CookingTime: "+CookTime);
-							String tags = driver.findElement(By.xpath("//div[@id = 'recipe_tags']")).getText();
-							System.out.println("------------------------");
-							String nutrients="";
-							try {
-								nutrients = driver.findElement(By.xpath("//table[@id='rcpnutrients']")).getText();
-
-							}catch( NotFoundException e) {
-								nutrients=" nutrients list not found";
-								logger.info(current_url, Recipe_name, ingredients, recipe_url, recipe_id, PrepTime, CookTime, tags, nutrients, e);
-							}
-
-							System.out.println("Nutrient List :"+nutrients);
-							System.out.println("------------------------");
-							String prepMethod = driver.findElement(By.xpath("//div[@id='recipe_small_steps']")).getText();
-							System.out.println("Prepration Method :"+prepMethod);
-							System.out.println("------------------------");
-							System.out.println("FoodCategory: "+getFoodCategory().toString());
-							System.out.println("------------------------");
-							System.out.println("RecipeCategory: "+getRecipeCategory().toString());
-							System.out.println("------------------------");
-							System.out.println("TargettedMorbidity: "+SetCoMorbidity().toString());
-						}
+							}//end of recipeSize if loop
+					}catch(IndexOutOfBoundsException e) {
+						System.out.println("Index Out of Bound Exception");
 						driver.navigate().back();
 						recipeLinks=driver.findElements(By.xpath("//div[@class='rcc_recipecard']//div[3]/span/a"));
+						continue;
 					}
-			}catch(IndexOutOfBoundsException e) {
-				System.out.println("Index Out of Bound Exception");
-				driver.navigate().back();
-				recipeLinks=driver.findElements(By.xpath("//div[@class='rcc_recipecard']//div[3]/span/a"));
-				continue;
-							}
-			total_recipes = total_recipes + recipeLinksSize;
-			System.out.println("Total Recipes:"+total_recipes);  
-			System.out.println("Eliminated Recipes:"+eliminated_recipe_count);
-			timer_end = Instant.now();
-			System.out.println("Time taken to execute in Minutes: "+ Duration.between(timer_start,timer_end).toMinutes());;
-		}
+
+					total_recipes = total_recipes + recipeLinksSize;
+					System.out.println("Total Recipes:"+total_recipes);  
+					System.out.println("Eliminated Recipes:"+eliminated_recipe_count);
+					timer_end = Instant.now();
+					System.out.println("Time taken to execute in Minutes: "+ Duration.between(timer_start,timer_end).toMinutes());
+
+				}//end of pages loop
+				getAlphabates=driver.findElements(By.xpath("//table[@id='ctl00_cntleftpanel_mnuAlphabets']//td[position()>=3 and position()<last()]//a"));
+			}//end of alphabet count loop
+		}//end of if alphabet count
 
 	}
-	public FoodCategory getFoodCategory() {
-
-		String tags = driver.findElement(By.xpath("//div[@id = 'recipe_tags']")).getText();
-		String ingredients = driver.findElement(By.xpath("//div[@id='rcpinglist']")).getText();
-
-		if (tags.toLowerCase().contains("jain")) {
-			return FoodCategory.JAIN;
-
-		} else if (ingredients.toLowerCase().contains("egg")) {
-			return FoodCategory.EGGETARIAN;
-		}
-		Pattern p= Pattern.compile("(milk|cheese|yogurt|icecream|buttermilk|paneer|ghee|chocolate|butter|dahi)");
-		Matcher regexMatcher = p.matcher(ingredients.toLowerCase());
-		if(!regexMatcher.find()) {
-			return FoodCategory.VEGAN;                
-		}
-		return FoodCategory.VEGETARIAN;
-	}
-
-	public RecipeCategory getRecipeCategory() {
-		String tags = driver.findElement(By.xpath("//div[@id = 'recipe_tags']")).getText();
-
-		if (tags.toLowerCase().contains("breakfast")) {
-			return RecipeCategory.BREAKFAST;
-
-		} else if (tags.toLowerCase().contains("lunch")) {
-			return RecipeCategory.LUNCH;
-
-		} else if (tags.toLowerCase().contains("snack")) {
-			return RecipeCategory.SNACK;
-		} else if (tags.toLowerCase().contains("dinner")) {
-			return RecipeCategory.DINNER;
-		}
-		return RecipeCategory.NONE;
-	}
-
-	public Comorbidity SetCoMorbidity() {
-
-		return Comorbidity.HYPOTHYROIDISM;
-
-	}	
 }
+
 
